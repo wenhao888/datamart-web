e40938a85d9d84fbb8ead9989ef69a6dc4c75cd1
wenhao <wenhao.lin@gmail.com>
2016-07-29 22:25:18 -0700

    organize code base

--- src/app/controllers/index.js
+++ src/app/controllers/index.js
@@ -3,34 +3,14 @@
 // it also sets up basic express routes
 
 import { Router } from 'express';
-import mongoose from 'mongoose';
-import extrasController from './extras';
-
-// load models
-const Movie = mongoose.model('Movie');
-
-// create router
 const router = Router();
-// load other controllers
-router.use('/extras', extrasController);
+
 
 // set basic routes
 router.get('/', (req, res, next) => res.render('index', {
   title: 'datamart'
 }));
-// note that `movies` will likely be empty because the local mongodb is empty.
-// you can populate it with example data by downloading it here:
-// https://gist.githubusercontent.com/thekelvinliu/152f2c488430be9b6649c963d5a2afea/raw/22d73b73fb653c091d4a5ffe470299d64a0d0fb7/movies
-// then navigate to the directory containing the file and run the following:
-// $ mongoimport --db test --collection movies movies
-router.get('/movies', (req, res, next) =>
-  Movie
-    .find()
-    .exec((err, movies) => (err) ? next(err) : res.render('movies', {
-      title: 'Movies!',
-      movies
-    }))
-);
+
 
 // export router
 export default router;
--- src/config.js
+++ src/config.js
@@ -3,14 +3,10 @@
 import path from 'path';
 
 export default {
-  // address of mongodb
-  db: process.env.MONGOURI || 'mongodb://localhost:27017/test',
-  user: process.env.DB_USER,
-  pass: process.env.DB_PASS,
   // environment
   env: process.env.NODE_ENV || 'development',
   // port on which to listen
-  port: process.env.PORT || 5000,
+  port: process.env.PORT || 3000,
   // path to root directory of this app
   root: path.normalize(__dirname)
 };
