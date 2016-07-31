fe14aa7189c8eebf95d7b9c5728a7b4d053ae338
wenhao <wenhao.lin@gmail.com>
2016-07-31 11:25:48 -0700

    save change before continue

--- src/app/controllers/index.js
+++ src/app/controllers/index.js
@@ -2,15 +2,15 @@
 
 
 import { Router } from 'express';
-import jsxRender from "../../core/jsxRender";
+import jsxRender  from "../../core/jsxRender";
+import respRender from "../../core/respRender";
 
 const router = Router();
 
 
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   var html = jsxRender.render("index/index.js", {});
-  res.render('index/index', { content: html})
-
+  respRender.render(res, {content: html});
 });
 
 
