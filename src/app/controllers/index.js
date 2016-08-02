d88ce219b734f623f7586c0bfe4e9eda9ff3fbbe
wenhao <wenhao.lin@gmail.com>
2016-08-02 00:56:55 -0700

    create vendor pack

--- src/app/controllers/index.js
+++ src/app/controllers/index.js
@@ -1,16 +1,28 @@
 'use strict';
 
-
+import React from "react";
 import { Router } from 'express';
 import fluxer from "fluxer";
-import index from "../views/index/index";
+
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import Index from "../views/index/index";
 import respRender from "../../core/respRender";
 
 const router = Router();
 
 
 router.get('/', (req, res) => {
-  var html = fluxer("index", index);
+  var html = fluxer("index", ()=> {
+      let store = createStore((state, action) => {
+          return state;
+      });
+      return (
+      <Provider store={store}>
+        <Index />
+      </Provider>)
+   });
+
   respRender.render(res, {content: html, script:'index_browser.js', style:"index.css"});
 });
 
--- src/app/views/index_browser.js
+++ src/app/views/index_browser.js
@@ -1,11 +1,19 @@
-
+import React from "react";
 import Index from "./index/index";
 import fluxerFactoy from "fluxer";
-import $ from "jquery";
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
 var fluxer = fluxerFactoy(document);
-fluxer.render("index", Index);
 
-$(document).ready(function() {
-   console.log("document is ready") ;
+
+fluxer.render("index", ()=> {
+   let store = createStore((state, action) => {
+      return state;
+   });
+   return (
+       <Provider store={store}>
+          <Index />
+       </Provider>)
 });
+
