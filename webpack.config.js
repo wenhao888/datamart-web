5f8ca617ccec269f2a5e044fd357cd34f589055c
wenhao <wenhao.lin@gmail.com>
2016-08-01 23:18:37 -0700

    improve server -side rendering

--- webpack.config.js
+++ webpack.config.js
@@ -6,27 +6,19 @@ var bower_dir = __dirname + '/bower_components';
 var config =  {
     entry :{
         index_browser: [path.join(__dirname, "/src/app/views/index_browser.js")],
-        vendors: ['react']
+        vendors: ['react','react-dom']
     },
     output: {
         path: path.join(__dirname, "/dist/public/views/"),
         filename: '[name].js'
     },
-    addVendor: function (name, path) {
-        this.resolve.alias[name] = path;
-        this.module.noParse.push(new RegExp(path));
-    },
     resolve: {
-        //alias: {
-        //    'react': bower_dir + '/react/react.min.js'
-        //},
         extensions: ['', '.js', '.jsx']
     },
     plugins: [
-       // new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js')
+       new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js')
     ],
     module: {
-        //noParse: [bower_dir + '/react/react.min.js'],
         loaders: [
             {
                 test: /\.jsx$/,
@@ -45,5 +37,4 @@ var config =  {
     }
 };
 
-//config.addVendor('react', bower_dir + '/react/react.min.js');
 module.exports = config;
