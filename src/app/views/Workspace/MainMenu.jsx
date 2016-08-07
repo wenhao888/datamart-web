0dd8c89971c5dfeb1fb34fab848f0553aa802507
wenhao <wenhao.lin@gmail.com>
2016-08-07 01:42:06 -0700

    add workspace page

--- src/app/views/Workspace/MainMenu.jsx
+++ src/app/views/Workspace/MainMenu.jsx
@@ -6,8 +6,8 @@ import { Router, Route, Link, browserHistory } from 'react-router'
 const MainMenus= (props)=> (
     <ul className="mainMenuList">
         <li>
-            <Link to="/workspace#profile"> Profile </Link>
-            <Link to="/workspace#dashboard"> Dashboard </Link>
+            <Link to="/workspace/profile"> Profile </Link>
+            <Link to="/workspace/dashboard"> Dashboard </Link>
         </li>
         <li>
            <a href="/"> Log out </a>
